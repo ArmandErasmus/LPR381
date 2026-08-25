65084f31926cfa4866cf08e947cde3360a2847f6
ArmandErasmus <armanderas10@gmail.com>
2026-08-25 12:09:08 +0200

    Added the .gitignore and the sensitivityanalysis

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR_381_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
